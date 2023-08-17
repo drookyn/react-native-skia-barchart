5e2b034f557ab23ed870b93c23fa05097aa931f5
Florian Vogt <florian.vogt@aurora-lifescience.de>
2023-08-17 21:12:34 +0200

    feat: add reanimated and skia

--- App.tsx
+++ App.tsx
@@ -6,13 +6,20 @@
  */
 
 import React from 'react';
-import {SafeAreaView, ScrollView} from 'react-native';
+import {Canvas, Circle, Group} from '@shopify/react-native-skia';
 
 function App(): JSX.Element {
+  const size = 256;
+  const r = size * 0.33;
+
   return (
-    <SafeAreaView>
-      <ScrollView contentInsetAdjustmentBehavior="automatic"></ScrollView>
-    </SafeAreaView>
+    <Canvas style={{flex: 1}}>
+      <Group blendMode="multiply">
+        <Circle cx={r} cy={r} r={r} color="cyan" />
+        <Circle cx={size - r} cy={r} r={r} color="magenta" />
+        <Circle cx={size / 2} cy={size - r} r={r} color="yellow" />
+      </Group>
+    </Canvas>
   );
 }
 
